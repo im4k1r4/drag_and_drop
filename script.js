f7f76ae0ece3c4e26fb30127b623fd4bc5c9192a
im4k1r4 <imthiagomartins@live.com>
2023-02-22 19:07:04 -0300

    Script em desenvolvimento.

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
-document.querySelector('.neutralArea').addEventListener('click', (e) => {
-    console.log('TARGET', e.target)
+document.querySelectorAll('.item').forEach(item => {
+    item.addEventListener('dragstart', dragStart); // quando inicio o drag
+    item.addEventListener('dragend', dragEnd); // quando solto o item
 });
+
+document.querySelectorAll('.area').forEach(area => {
+    area.addEventListener('dragover', dragOver); // fn roda quando estiver arrastando o item e o item passar por cima da area selecionada
+    area.addEventListener('dragleave', dragLeave); //
+    area.addEventListener('drop', drop); //
+})
+
+
+// Funções item
+function dragStart(e) {
+    e.currentTarget.classList.add('dragging'); // classe dragging dá opacidade via css
+}
+
+function dragEnd(e) {
+    e.currentTarget.classList.remove('dragging');
+}
+
+// Funções área
+function dragOver(e) {
+    console.log('Passou por cima')
+}
+
+function dragLeave() {
+    
+}
+
+function drop() {
+    
+}
+
+//36:01
